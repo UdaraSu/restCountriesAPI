02ba54cc87e3dba4c8f7de97bc3943e0735dcb4f
Udara Ireshan <155006912+UdaraSu@users.noreply.github.com>
2025-04-11 00:27:30 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-pink-600 text-3xl font-bold text-center mt-10">
+      🌍 Hello from Tailwind + React!
+    </div>
+  );
+}
+
+export default App;
